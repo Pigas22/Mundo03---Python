4c8126502f77238201151b6e4e5f8bfec770c205
Thiago Holz Coutinho <125706324+Pigas22@users.noreply.github.com>
2023-04-10 17:25:51 +0000

    Na pasta Aula22_Modulos_Pacotes, estão os exemplos utilizados na aula pelo Prof. Gustavo Guanabara.
    No arquivo Aula22, tem um resumo com minhas palavras sobre o que é falado na Aula 22 do curso.

---
+++ Aulas/Aula22.py
@@ -0,0 +1,18 @@
+# Aula 22 – Módulos e Pacotes
+
+"""
+Aprendendo sobre Módulos:
+
+Criar um novo diretótio para criar modulos.
+
+Aprendendo a fazer o 'link' de arquivos .py
+
+ -> Importando funções de outro arquivo.py para o Código Principal.
+    - Exemplo: import uteis
+
+
+Aprendendo sobre Pacotes:
+
+Dentro dos Pacotes podemos ter vários Módulos nichados com temas especifícos, por exemplo: um módulo só para números, datas, strings ou cores.
+"""
+
