973a4226480cac50b324a59a6847589118ac227f
Thiago Holz Coutinho <125706324+Pigas22@users.noreply.github.com>
2023-04-18 13:18:08 +0000

    Realizei a coloração do sistema, utilizando uma função, chamada "cores", para facilitar o processo.

--- Atividades/Ativ115a/comandos/menu/__init__.py
+++ Atividades/Ativ115a/comandos/menu/__init__.py
@@ -1,15 +1,24 @@
 def linha_simples():
-  print(40 * '-')
+  print(cores(1, 35) + 40 * '-' + cores())
 
 
 def linha_composta():
-  print(20 * '-=')
+  print(cores(1, 35) + 20 * '-=' + cores())
 
 
 def cabecalho(nome):
   linha_composta()
-  print(f'{nome:^40}')
+  print(cores (1, 34) + f'{nome:^40}' + cores())
   linha_composta()
+
+
+def cores(estilo=0, texto=0, fundo=0):  
+  if fundo == 0:
+    return f'\033[{estilo};{texto}m'
+
+  else:
+    return f'\033[{estilo};{texto};{fundo}m'
+  
 
 
 def menu():  
@@ -20,14 +29,14 @@ def menu():
     cabecalho('MENU PRINCIPAL')
     
     # Tabela
-    print('1 - Ver pessoas cadastradas' + 
-          '\n' + '2 - Cadastrar uma nova pessoa' +
-          '\n' + '3 - Sair do Sistema')
+    print(cores(1, 35) + '1' + cores(1, 36) + ' - ' + cores(0, 34) + 'Ver pessoas cadastradas' + 
+          '\n' + cores(1, 35) + '2' + cores(1, 36) + ' - ' + cores(0, 34) + 'Cadastrar uma nova pessoa' +
+          '\n' + cores(1, 35) + '3' + cores(1, 36) + ' - ' + cores(0, 34) + 'Sair do Sistema' + cores())
     linha_simples()
 
     # Escolha
     try:
-      escolha = int(input('O que deseja fazer: '))
+      escolha = int(input(cores(1, 36) + ' -> O que deseja fazer: '))
       
       # Opção 1
       if escolha == 1:
@@ -44,9 +53,9 @@ def menu():
 
       # Erro
       else:
-        print('ERROR: Por favor digite um número entre [1, 2, 3]')
+        print(cores(1, 31) + 'ERROR: Por favor digite um número entre [1, 2, 3]')
   
     # Tratamento de Erro
     except Exception as error:
-      print(f'Encontramos um erro: {error}')
+      print(cores(1, 31) + f'Encontramos um erro: {error}')
   
